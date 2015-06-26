bb8547d82ed04d2146c8ca332eb0812acd512ea5
Chris Cornutt <enygma@phpdeveloper.org>
2015-06-26 17:46:41 -0400

    Updates to the route container for docblocks and bugfixes

--- src/Psecio/Invoke/RouteContainer.php
+++ src/Psecio/Invoke/RouteContainer.php
@@ -4,21 +4,49 @@ namespace Psecio\Invoke;
 
 class RouteContainer
 {
+    /**
+     * Current route (match) instance
+     * @var \Psecio\Invoke\MatchInstance
+     */
     private $route;
+
+    /**
+     * Current configuration
+     * @var array
+     */
     private $config;
 
-    public function __construct($route, $config)
+    /**
+     * Initialize the container with the provided route and
+     *     configuration information
+     *
+     * @param string $route Route to provide to the match
+     * @param array $config Route configuration
+     */
+    public function __construct($route, array $config)
     {
         $this->route = \Psecio\Invoke\Match::create('route.regex',
             array('route' => $route));
         $this->config = $config;
     }
-    public function getPath()
-    {
-        return $this->reoute->getRoute();
-    }
+
+    /**
+     * Get the current configuration
+     *
+     * @return array Configuration set
+     */
     public function getConfig()
     {
         return $this->config;
+    }
+
+    /**
+     * Get the current route instance
+     *
+     * @return \Psecio\Invoke\MatchInstance
+     */
+    public function getRoute()
+    {
+        return $this->route;
     }
 }
