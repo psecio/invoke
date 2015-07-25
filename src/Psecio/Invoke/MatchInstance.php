d4566820c1d4e1abe69c877b75fcbcfe6ff355ca
Chris Cornutt <enygma@phpdeveloper.org>
2015-07-24 21:13:25 -0500

    adding docblocks to MatchInstance class

--- src/Psecio/Invoke/MatchInstance.php
+++ src/Psecio/Invoke/MatchInstance.php
@@ -4,18 +4,46 @@ namespace Psecio\Invoke;
 
 abstract class MatchInstance
 {
+	/**
+	 * Current configuration details
+	 * @var array
+	 */
 	protected $config = array();
+
+	/**
+	 * Flag to negate the check
+	 * @var boolean
+	 */
 	protected $negate = false;
 
+	/**
+	 * Init the object with the given confgiuration and negation flag
+	 *
+	 * @param array $config Configuration information
+	 * @param boolean $negate Negation flag, default false [optional]
+	 */
 	public function __construct(array $config = array(), $negate = false)
 	{
 		$this->setConfig($config);
 	}
 
+	/**
+	 * Set the current configuration values
+	 *
+	 * @param array $config Configuration information
+	 */
 	public function setConfig(array $config)
 	{
 		$this->config = $config;
 	}
+
+	/**
+	 * Get the current configuration, or if a key is provided
+	 * 	try to locate that one value
+	 *
+	 * @param string $key Key to locate [optional]
+	 * @return mixed Either entire config or key is found
+	 */
 	public function getConfig($key = null)
 	{
 		if ($key !== null) {
@@ -42,5 +70,11 @@ abstract class MatchInstance
 		return $message;
 	}
 
+	/**
+	 * Evaluate the match
+	 *
+	 * @param \Psecio\Invoke\Data $data Object instance
+	 * @return boolean Result of evaluation
+	 */
 	abstract public function evaluate($data);
 }
